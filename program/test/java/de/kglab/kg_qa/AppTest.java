59432242d7830b7d522fe86e3cfd7f80d57c4587
Dudekaiser <33812854+Dudekaiser@users.noreply.github.com>
2019-01-15 14:19:47 +0100

    Create AppTest.java

---
+++ program/test/java/de/kglab/kg_qa/AppTest.java
@@ -0,0 +1,21 @@
+package de.kglab.kg_qa;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("Hello world");
+    }
+}
